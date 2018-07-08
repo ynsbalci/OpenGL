7c385f1bff052f91c7dc8b1cc2482fc72cd0176f
YunusBALCI <ynsbalci@outlook.com>
2018-07-08 12:11:06 +0300

    using modern open gl
    include glew
    setting properties

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,4 +1,7 @@
+#include <GL\glew.h>
 #include <GLFW/glfw3.h>
+
+#include <iostream>
 
 int main(void)
 {
@@ -7,6 +10,8 @@ int main(void)
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
+
+	
 
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
@@ -19,11 +24,23 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	if (glewInit() != GLEW_OK) {
+		std::cout << "Error !" << std::endl;
+	}
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glBegin(GL_TRIANGLES);
+		glVertex2f(-0.5f, -0.5f);
+		glVertex2f(0.0f, 0.5f);
+		glVertex2f(0.5f, -0.5f);
+		glEnd();
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
