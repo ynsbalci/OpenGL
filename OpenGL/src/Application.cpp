b2ccc17b00dbfe4859bb0c6e0c789a94a69978de
YunusBALCI <ynsbalci@outlook.com>
2018-07-15 19:16:47 +0300

    10.Dealing with11.Uniforms in OpenGL Errors in OpenGL

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -120,6 +120,8 @@ int main(void)
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 
+	glfwSwapInterval(1);
+
 	if (glewInit() != GLEW_OK) {
 		std::cout << "Error !" << std::endl;
 	}
@@ -154,24 +156,36 @@ int main(void)
 
 
 	ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
-	
-	//std::cout << "VERTEX" << std::endl;
-	//std::cout << source.VertexSource << std::endl;
-	//std::cout << "FRAGMENT" << std::endl;
-	//std::cout << source.FragmentSource << std::endl;
-
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
 	glUseProgram(shader);
+
+	int location = glGetUniformLocation(shader, "u_Color");
+	ASSERT(location != -1);
+
+	glUniform4f(location, 0.2f, 0.3f, 0.8f, 1.0f);
+	
+	float r = 0.0f;
+	float increment = 0.05f;
 
 	/* Loop until the user closes the window */ 
 	while (!glfwWindowShouldClose(window))
 	{
+
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		glUniform4f(location, r, 0.3f, 0.8f, 1.0f);
 		//GLClearError();
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 		//ASSERT(GLLogCall());
+
+		if (r > 1.0f) {
+			increment = -0.05f;
+		}
+		else if (r < 0.0f) {
+			increment = 0.05f;
+		}
+		r += increment;
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
